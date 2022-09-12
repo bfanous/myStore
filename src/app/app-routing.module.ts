fac6373342fc4ebdb9143fbae465ce5b569081da
bfanous <beshoykamaf@gmail.com>
2022-09-12 11:23:57 +0200

    adding components
    Signed-off-by: bfanous <beshoykamaf@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
+import { HomeComponent } from './home/home.component';
+import { CardListComponent } from './card-list/card-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'cardList', component: CardListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
